e17786ebec068cb15e496013be6e4490c7ef0e98
Pedro Roman Andreis <pedro13andreis@gmail.com>
2024-09-05 20:02:56 -0300

    Update Main_TumblyTowers.java

--- src/com/scs/trickytowers/Main_TumblyTowers.java
+++ src/com/scs/trickytowers/Main_TumblyTowers.java
@@ -159,7 +159,7 @@ public class Main_TumblyTowers implements ContactListener, KeyListener {
 			Edge rightEdge = new Edge(this, this.getRightBucketPos(player.id_ZB), (float)(Statics.WORLD_HEIGHT_LOGICAL-10), this.getRightBucketPos(player.id_ZB), (float)(Statics.WORLD_HEIGHT_LOGICAL/2));
 			this.addEntity(rightEdge);*/
 
-				VibratingPlatform v = new VibratingPlatform(this, this.getCentreBucketPos(player.id_ZB), (float)(Statics.WORLD_HEIGHT_LOGICAL-20), bucketWidth*0.9f);
+				VibratingPlatform v = new VibratingPlatform(this, this.getCentreBucketPos(player.id_ZB), (float)(Statics.WORLD_HEIGHT_LOGICAL-15), bucketWidth*0.5f);
 				this.addEntity(v);
 
 				player.vib = v;
